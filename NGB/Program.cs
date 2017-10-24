1889d26f5cd460b3b0599bf2ab5dfafc973ac40c
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-10-24 16:24:45 -0200

    Adding NBG logo as an icon in the home page.

--- NGB/Program.cs
+++ NGB/Program.cs
@@ -16,7 +16,7 @@ namespace NGB
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Home());
         }
     }
 }
