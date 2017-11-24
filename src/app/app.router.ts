6b27208b0ae678bb0fe2da108e090f4be4e81e6a
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-11-24 15:46:01 -0200

    Moving side menu component to app component. Also, creating product component.

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -2,12 +2,12 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 /* Imported custom components */
-import { SideMenuComponent } from '../pages/side-menu/side-menu.component';
+import { ProductComponent } from '../pages/product/product.component';
 
 /* Constant used to export routes to app.module */
 export const router: Routes = [
   {path: '', redirectTo:'home', pathMatch: 'full'},
-  {path: 'home', component: SideMenuComponent}
+  {path: 'home', component: ProductComponent}
 ];
 
 export const routes: ModuleWithProviders = RouterModule.forRoot(router);
