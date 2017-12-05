79c83515c1bef397fb04816999229460f5bc3147
Matheus Joranhezon <mjoranhezon@gmail.com>
2017-12-05 14:43:23 -0200

    Creating product component. Still need to make header, list and registration.

--- src/pages/product/product.component.ts
+++ src/pages/product/product.component.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: 'product.component.html'
+  templateUrl: 'product.component.html',
+  styleUrls: ['./product.component.css']
 })
 
 export class ProductComponent {
